4f6dacb1223b13e27b125a32e22aa359e90c8f97
Sidaartha Reddy <sidaarthareddy1998@gmail.com>
2020-05-03 08:49:06 +0530

    added auto function

--- extract_to_json.py
+++ extract_to_json.py
@@ -5,10 +5,10 @@ from bs4 import BeautifulSoup
 import os
 from os.path import isfile, isdir, join, exists
 
-class ExtractToJSON(object):
-	"""docstring for ExtractToJSON"""
+class ExtractData(object):
+	"""docstring for ExtractData"""
 	def __init__(self, dir_path="YouTubeData"):
-		super(ExtractToJSON, self).__init__()
+		super(ExtractData, self).__init__()
 		self._dir_path = dir_path
 		self._dirs_dict = {
 			'history': "history",
@@ -76,39 +76,92 @@ class ExtractToJSON(object):
 			history_list = self.load_json("DataJSON/watch_history.json")
 
 		history_df = pd.DataFrame(history_list)
-		channel_list = list(set(history_df['channel_title'].tolist()))
-		unique_videos = list(set(history_df['video_title'].tolist()))
+		unique_channels = list(set(history_df['channel_title']))
+		unique_videos = list(set(history_df['video_title']))
 
 		history_stats_dict = {
 			'videos': {
-				'total_video_count': len(history_list),
-				'unique_video_count': len(unique_videos)
+				'stats': {
+					'total_video_count': len(history_list),
+					'unique_video_count': len(unique_videos)
+				}
 			},
 			'channels': {
-				'channel_count': len(channel_list),
-				'channel_list': channel_list
+				'stats': {
+					'channel_count': len(unique_channels)
+				}
 			}
 		}
 
-		for channel in channel_list:
+		for channel in unique_channels:
 			matched_df = history_df.loc[history_df['channel_title'] == channel]
 			history_stats_dict['channels'][channel] = {
+				'channel': channel,
 				'view_count': matched_df.shape[0],
+				'videos_count': len(set(matched_df['video_title'])),
 				'videos_list': list(set(matched_df['video_title'])),
-				'url': matched_df['channel_url'].tolist()[0]
+				'channel_url': matched_df['channel_url'].tolist()[0]
 			}
 
 		for video in unique_videos:
 			matched_df = history_df.loc[history_df['video_title'] == video]
 			history_stats_dict['videos'][video] = {
+				'video': video,
 				'view_count': matched_df.shape[0],
-				'url': matched_df['video_url'].tolist()[0]
+				'video_url': matched_df['video_url'].tolist()[0],
+				'channel': matched_df['channel_title'].tolist()[0],
+				'channel_url': matched_df['channel_url'].tolist()[0]
 			}
 
 		with open('DataJSON/watch_history_stats.json', 'w') as f:
 		     json.dump(history_stats_dict, f, ensure_ascii=False, indent=4)
 		print("JSON file saved to DataJSON/watch_history_stats.json")
 
+	def get_watch_history_stats_csv(self):
+		if not exists("DataJSON/watch_history.json"):
+			history_list = self.get_watch_history_json()
+		else:
+			history_list = self.load_json("DataJSON/watch_history.json")
+
+		history_df = pd.DataFrame(history_list)
+		unique_channels = list(set(history_df['channel_title']))
+		unique_videos = list(set(history_df['video_title']))
+
+		channels_list = []
+		for channel in unique_channels:
+			matched_df = history_df.loc[history_df['channel_title'] == channel]
+			channels_list.append({
+				'channel': channel,
+				'view_count': matched_df.shape[0],
+				'videos_count': len(set(matched_df['video_title'])),
+				'channel_url': matched_df['channel_url'].tolist()[0]
+			})
+		channels_df = pd.DataFrame(channels_list)
+		channels_df.to_csv("DataCSV/watch_history_channel_stats.csv", sep=',', index=False)
+		print("CSV file saved to DataCSV/watch_history_channel_stats.csv")
+
+		videos_list = []
+		for video in unique_videos:
+			matched_df = history_df.loc[history_df['video_title'] == video]
+			videos_list.append({
+				'video': video,
+				'view_count': matched_df.shape[0],
+				'video_url': matched_df['video_url'].tolist()[0],
+				'channel': matched_df['channel_title'].tolist()[0],
+				'channel_url': matched_df['channel_url'].tolist()[0]
+			})
+		videos_df = pd.DataFrame(videos_list)
+		videos_df.to_csv("DataCSV/watch_history_video_stats.csv", sep=',', index=False)
+		print("CSV file saved to DataCSV/watch_history_video_stats.csv")
+
+	def auto(self):
+		if not exists("DataJSON/watch_history.json"):
+			self.get_watch_history_json()
+
+		self.get_watch_history_stats_json()
+		self.get_watch_history_stats_csv()
+
+
 if __name__ == '__main__':
-	data_obj = ExtractToJSON(dir_path="YouTubeData")
-	data_obj.get_watch_history_stats_json()
+	data_obj = ExtractData(dir_path="YouTubeData")
+	data_obj.auto()
